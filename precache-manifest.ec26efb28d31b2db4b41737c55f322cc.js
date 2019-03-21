c89b8f50b73be06346d850ad61fc22de633bffa9
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2019-03-21 10:14:51 -0400

    Updates

---
+++ precache-manifest.ec26efb28d31b2db4b41737c55f322cc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a4a32963a1766e98cf49",
+    "url": "/Budget-App/static/css/main.ec8525f4.chunk.css"
+  },
+  {
+    "revision": "a4a32963a1766e98cf49",
+    "url": "/Budget-App/static/js/main.bffeb8b6.chunk.js"
+  },
+  {
+    "revision": "865771a299417aef8f67",
+    "url": "/Budget-App/static/js/runtime~main.fe220f5b.js"
+  },
+  {
+    "revision": "9a77bcef78263da5fe99",
+    "url": "/Budget-App/static/css/2.a5cb4f40.chunk.css"
+  },
+  {
+    "revision": "9a77bcef78263da5fe99",
+    "url": "/Budget-App/static/js/2.5c0282d8.chunk.js"
+  },
+  {
+    "revision": "9efcb360daa8911a166df24b80dd9f57",
+    "url": "/Budget-App/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.485cefd38945c977dc26dfb9d09c89ce.js"
+  "/Budget-App/precache-manifest.ec26efb28d31b2db4b41737c55f322cc.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/Budget-App/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
